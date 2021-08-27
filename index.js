df58a052cbf557124cc394bf20e2772f7e70f3b4
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-27 19:18:46 -0400

    Done!

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
+let obj = {}
+
 // Add your code here
+function submitData(name, email) {
+
+    obj.name = name
+    obj.email = email
+
+    let url = "http://localhost:3000/users"
+    return fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json',
+        Accept: "application/json", },
+        body:JSON.stringify(obj)
+    })
+    .then(Response  => Response.json())
+    .then(body => {
+
+        console.log(body.id)
+        let p = document.createElement('p')
+        p.innerHTML = body.id
+        document.body.appendChild(p)
+        
+    })
+    .catch (function (error) {
+        let p = document.createElement('p')
+        console.log(error.message)
+        p.innerHTML = error.message
+        document.body.appendChild(p)
+
+     })
+
+    
+}
+
+
+
+
+    pa = document.createElement('p')
+    pa.innerText = "got"
+    pa.addEventListener('click', submitData)
+    document.body.appendChild(pa)
+
+    
